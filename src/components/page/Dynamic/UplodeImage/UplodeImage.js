23a429b3e2bca815baeb918c0519ce06aa88a467
rahul-sarker18 <rahulsarker1495@gmail.com>
2023-03-13 21:48:40 +0530

    add

--- src/components/page/Dynamic/UplodeImage/UplodeImage.js
+++ src/components/page/Dynamic/UplodeImage/UplodeImage.js
@@ -9,41 +9,58 @@ const UplodeImage = () => {
     watch,
     formState: { errors },
   } = useForm();
-  const onsubmite = (data) => {};
+  const handleAddProduct = (data) => {
+    const img = data.image[0];
+    console.log(img);
+  };
 
   return (
     <div>
-      <div
-        className="lg:w-2/5 mx-auto my-8 shadow-2xl"
-      >
-        <form className="p-4" onClick={handleSubmit(onsubmite)}>
-          <div className="w-full flex flex-col gap-1">
-            <label
-              htmlFor="image"
-              className="text-xl capitalize font-semibold "
-            >
-              Upload Image <span className="text-red-600">*</span>
-            </label>
-            <label
-              htmlFor="image"
-              className="flex items-center gap-4 px-2 py-4 w-full 
+      <div className="lg:w-2/5 mx-auto my-8 shadow-2xl">
+        
+          <div className=" w-full">
+            <div className=" my-5 flex items-center justify-center">
+              <form onSubmit={handleSubmit(handleAddProduct)} className="">
+                <div className="grid grid-cols-1  ">
+                  <div className="w-full flex flex-col gap-1">
+                    <label
+                      htmlFor="image"
+                      className="text-xl capitalize font-semibold my-3 "
+                    >
+                      Upload Image <span className="text-red-600">*</span>
+                    </label>
+                    <label
+                      htmlFor="image"
+                      className="flex items-center gap-4 px-2 py-4 w-full 
                        border-2 border-dashed"
-            >
-              <RiImageAddFill className="w-16  h-16 text-primary"></RiImageAddFill>
-              <input
-                type="file"
-                id="image"
-                placeholder="image"
-                className="placeholder: text-base "
-                {...register("image", {
-                  required: "must upload an image",
-                })}
-              />
-            </label>
-            {errors.image && <p>{errors.image.message}</p>}
+                    >
+                      <RiImageAddFill className="w-16  h-16 text-primary"></RiImageAddFill>
+                      <input
+                        type="file"
+                        id="image"
+                        placeholder="image"
+                        className="placeholder: text-base "
+                        {...register("image", {
+                          required: "must upload an image",
+                        })}
+                      />
+                    </label>
+                    {errors.image && <p>{errors.image.message}</p>}
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-center  mt-4">
+                  <button
+                    type="submit"
+                    className="btn btn-primary my-6 "
+                  >
+                     Post
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
-          <button className="btn btn-primary my-3">submit</button>
-        </form>
+        
       </div>
     </div>
   );
