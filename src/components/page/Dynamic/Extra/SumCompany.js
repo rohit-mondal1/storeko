8dab130c7b740f1444b3eecb930d4039213e5c7c
rahul-sarker18 <rahulsarker1495@gmail.com>
2023-03-14 23:30:32 +0530

    marquee section make

--- src/components/page/Dynamic/Extra/SumCompany.js
+++ src/components/page/Dynamic/Extra/SumCompany.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Marquee from 'react-fast-marquee';
 import './Style.css'
 
 const SumCompany = () => {
-    return (
-        <div>
-            <Marquee direction="right" speed={100} delay={5}>
-          <div className="image_wrapper">
-            <a target='blank' href="/google.com">
+   const [data , setData] = useState([])
+   useEffect(()=>{
+    fetch('http://localhost:8000/marqe')
+    .then(res => res.json())
+    .then(dat => setData(dat))
+    
+   },[])
 
-            <img src="https://i.ibb.co/gyRyJ6k/download.jpg" alt="" />
+    return (
+        <div className='my-10'>
+            <Marquee direction="right" speed={100} delay={5}>
+              {data.map(item=>
+              <div key={item._id} className="image_wrapper">
+            <a target='blank' href={item.link}>
+
+            <img title={item.name} src={item.image} alt={item.name} />
             </a>
-          </div>
+          </div> )}
+          
           
         </Marquee>
         </div>
